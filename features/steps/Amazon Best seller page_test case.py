bc7753408be82c8541cd91272cf5285d7fc887a3
kazijesin <69095694+kazijesin@users.noreply.github.com>
2021-06-13 00:37:46 -0400

    Amazon best seller page

---
+++ features/steps/Amazon Best seller page_test case.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+
+
+@given('Open Amazon page')
+def open_amazon(context):
+    context.driver.get("https://www.amazon.com/")
+
+
+@when('Click {best seller}')
+def best_seller(context):
+    context.driver.find_element(By.CSS_SELECTOR, "a.nav-a").click()
+
+
+@then('Verify page display five links')
+def page_display(context):
+    actual_result = context.driver.find_element(By.CSS_SELECTOR, 'li.zg_selected')
+    expected_result = "five links"
+    assert expected_result == actual_result, f'Expected {expected_result}, but got{actual_result}'
+
+    context.driver.quit()
