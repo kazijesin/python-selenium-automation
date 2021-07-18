6abb109fea9d9e5c6a5e867ea1dbd62c4d848d46
kazijesin <69095694+kazijesin@users.noreply.github.com>
2021-07-17 21:17:28 -0400

    Add files via upload

--- search_result_page.py
+++ search_result_page.py
@@ -1,28 +1,11 @@
-from pages.base_page import Page
+from base_page import Page
 from selenium.webdriver.common.by import By
-
 
 class SearchResults(Page):
     SEARCH_RESULT = (By.XPATH, "//span[@class='a-color-state a-text-bold']")
-    PRODUCT_RESULT = (By.XPATH, "//a[@class='a-link-normal a-text-normal']")
-    SEARCH_RESULT1 = (By.CSS_SELECTOR, 'input#twotabsearchtextbox')
-    PRODUCT_RESULT1 = (By.CSS_SELECTOR, 'span.a-size-base-plus a-color-base a-text-normal')
-    SEARCH_RESULT2 = SIGN_IN = (By.CSS_SELECTOR,'h1.a-spacing-small')
 
-
-
-
-    def click_first_product(self):
-        self.click(*self.PRODUCT_RESULT)
-
-    def verify_search_worked(self, expected_text):
-        self.verify_text(expected_text, *self.SEARCH_RESULT)
-
-
-    def verify_search_worked_1(self, expected_text):
-        self.verify_text(expected_text, *self.SEARCH_RESULT2)
-
-
-
-
+    def verify_search_worked(self):
+        actual_result = self.driver.find_element(*self.SEARCH_RESULT).text
+        expected_result = 'Table'
+        assert expected_result == actual_result, f'Expected {expected_result}, but got {actual_result}'
 
